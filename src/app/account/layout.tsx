cf94391ea8a97c505daf755bba01a52a8bb72759
Golgotha Aksanti <goloking777@gmail.com>
2024-03-15 03:01:21 +0300

    bg(build): fix build

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -15,7 +15,7 @@ const AccountLayout = ({
 
   const router = useRouter();
 
-  if(!auth && !user) router.push("/")
+  // if(!auth && !user) router.push("/")
 
   return (
     <div className="relative w-10/12 mx-auto flex flex-col lg:grid grid-cols-7 gap-5 min-h-[80vh] text-xs mt-10">
